2f44aafcd74fd423771b4de724e4ade08b4f8be6
Kali Prasad <kprasadvnsi@protonmail.com>
2022-03-04 16:49:59 +0530

    remove async_std dev-dependency

--- examples/example.rs
+++ examples/example.rs
@@ -2,28 +2,27 @@
 // SPDX-License-Identifier: MIT
 
 extern crate packagekit;
-
 use packagekit::PackageKitProxy;
-use std::error::Error;
 
-#[async_std::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let connection = zbus::Connection::system().await?;
+fn main() -> zbus::Result<()> {
+    futures::executor::block_on(async move {
+        let connection = zbus::Connection::system().await?;
 
-    let pk = PackageKitProxy::new(&connection).await?;
+        let pk = PackageKitProxy::new(&connection).await?;
 
-    println!(
-        "Server version: {}.{}.{}",
-        pk.version_major().await?,
-        pk.version_minor().await?,
-        pk.version_micro().await?
-    );
-    println!(
-        "Backend: {} {}",
-        pk.backend_description().await?,
-        pk.backend_name().await?
-    );
-    println!("Supported MIME types:{:#?}", pk.mime_types().await?);
+        println!(
+            "Server version: {}.{}.{}",
+            pk.version_major().await?,
+            pk.version_minor().await?,
+            pk.version_micro().await?
+        );
+        println!(
+            "Backend: {} {}",
+            pk.backend_description().await?,
+            pk.backend_name().await?
+        );
+        println!("Supported MIME types:{:#?}", pk.mime_types().await?);
 
-    Ok(())
+        Ok(())
+    })
 }
